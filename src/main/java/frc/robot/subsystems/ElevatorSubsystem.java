e0a8c40c07262a48667778eaaf4950682066d1e3
FRC Team 1764 <first1764@gmail.com>
2025-01-11 15:37:18 -0600

    no more red text in elevator

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -6,33 +6,37 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.controls.StrictFollower;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
-import frc.robot.libraries.internal.LazyTalonFX;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.*;
 
 public class ElevatorSubsystem extends SubsystemBase {
   /** Creates a new ElevatorSubsystem. */
 
-  public LazyTalonFX elevatorMotor1;
-  public LazyTalonFX elevatorMotor2;
+  public TalonFX elevatorMotor1;
+  public TalonFX elevatorMotor2;
   public PIDController pidController;
   public DigitalInput limitSwitch;
-  public DigitalInput limitSwitch2;
   private PositionDutyCycle setVoltage;
   private PositionDutyCycle setVoltage2;
   private double desiredEncoder;
   
   int negative;
   public ElevatorSubsystem() {
-    elevatorMotor1 = new LazyTalonFX(Constants.ELEVATOR_MOTOR1.id, Constants.ELEVATOR_MOTOR1.busName);
-    elevatorMotor2 = new LazyTalonFX(Constants.ELEVATOR_MOTOR2.id, Constants.ELEVATOR_MOTOR2.busName);
-    elevatorMotor2.follow(elevatorMotor1);
+    elevatorMotor1 = new TalonFX(Constants.ELEVATOR_MOTOR1.id, Constants.ELEVATOR_MOTOR1.busName);
+    elevatorMotor2 = new TalonFX(Constants.ELEVATOR_MOTOR2.id, Constants.ELEVATOR_MOTOR2.busName);
+    elevatorMotor2.setControl(new StrictFollower(elevatorMotor1.getDeviceID()));
     limitSwitch = new DigitalInput(Constants.CLIMBER_SWITCH_LEFT);
     negative = 1;
     
@@ -65,7 +69,7 @@ public class ElevatorSubsystem extends SubsystemBase {
     elevatorMotor2.getConfigurator().apply(config2);
   }
   
-  public void elevatorOn(double desiredEncoderValue, double velocity){
+  public void elevatorOn(double desiredEncoderValue){
     desiredEncoder = desiredEncoderValue;
 
     double variable = pidController.calculate(getEncoderValue(),desiredEncoderValue);
@@ -77,8 +81,7 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
 
     variable = negative*Math.min(7.2, Math.abs(variable));
-    elevatorMotor1.set(ControlMode.PercentOutput,variable);  
-    elevatorMotor2.set(ControlMode.PercentOutput,variable);  
+    elevatorMotor1.set(variable);  
     
   }
   public double getDesiredEncoder(){
@@ -91,9 +94,31 @@ public class ElevatorSubsystem extends SubsystemBase {
   public double getEncoderValue2() {
     return elevatorMotor2.getPosition().getValueAsDouble();
   }
+  
+  public void zeroEncoder1() {
+    elevatorMotor1.setPosition(0);
+  }
+
+  public void zeroEncoder2() {
+    elevatorMotor2.setPosition(0);
+  }
+  
+  public boolean getLimitSwitch(){
+    return limitSwitch.get();
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putBoolean("ElevatorSwitch", getLimitSwitch());
+    SmartDashboard.putNumber("rightEncoder", getEncoderValue());
+    SmartDashboard.putNumber("leftEncoder", getEncoderValue2());
+    
+    zeroEncoder2();
+    zeroEncoder1();
+
+    elevatorMotor1.getPosition().refresh();
+    elevatorMotor2.getPosition().refresh();
+  
   }
 }
