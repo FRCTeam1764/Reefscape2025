bacf3b82c3653483e73f1547caf951e485b20b0a
FRC Team 1764 <first1764@gmail.com>
2025-01-13 16:18:11 -0600

    update to 2025 - merge into main later

--- src/main/java/frc/robot/commands/DriveCommands/DoA180.java
+++ src/main/java/frc/robot/commands/DriveCommands/DoA180.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.constants.SwerveConstantsYAGSL;
 import frc.robot.libraries.external.io.json.Rotation2JsonHandler;
 import frc.robot.subsystems.LimelightSubsystem;
