2ef41c694c7b2f9c81d62cde704596848a3dcf64
Team1764 <First1764@gmail.com>
2025-01-27 19:43:17 -0600

    fixed efficiency/kind of fixed cancoder/experimental gobackdefault

--- src/main/java/frc/robot/commands/ComplexCommands/CoralIntakePrep.java
+++ src/main/java/frc/robot/commands/ComplexCommands/CoralIntakePrep.java
@@ -26,13 +26,14 @@ public class CoralIntakePrep extends SequentialCommandGroup {
     ParallelDeadlineGroup grab = new ParallelDeadlineGroup(
       new WaitCommand(2), 
       new IntakeCommand(intake, 0.3));
-
+//TODO: NEED TO ENSURE ELAVTOR MOVES DOWN LOW AFTER WRIST IS CLEAR, CURRENTLY WILL NOT 
+//IF POSITION IS NOT GREATER THAN 20 
     SequentialCommandGroup up = new SequentialCommandGroup(
       new ElevatorCommand(elevator, CommandConstants.ELEVATOR_STOP_SAFE, false),
       new ParallelCommandGroup(
         new WristCommand(intake, CommandConstants.WRIST_UP),
         new ElevatorCommand(elevator, 0, true)
-          .onlyIf(() -> (intake.getEncoderPos()>20)) //TODO: fix half out encoder
+          .onlyIf(() -> (intake.getEncoderPos()>20)) //TODO: fix half out encoder MOVE NUMBER INTO CONSTANTS
       )    
     );
     
