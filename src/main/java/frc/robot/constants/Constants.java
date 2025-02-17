6458a933c5c0081e6c5c631a2e7a0437cb95226b
FIRSTteam1764 <first1764@gmail.com>
2025-02-17 10:38:06 -0600

    motor ids

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -8,13 +8,13 @@ public final class Constants {
     public static final int PRESSURE_SENSOR_PORT =   0;
 
 //TODO: FIND PORTS
-    public static final CanPort ELEVATOR_MOTOR1 =     new CanPort(0);
-    public static final CanPort ELEVATOR_MOTOR2 =     new CanPort(0); 
+    public static final CanPort ELEVATOR_MOTOR1 =     new CanPort(61); // links
+    public static final CanPort ELEVATOR_MOTOR2 =     new CanPort(62); // rechts
 
-    public static final CanPort CLIMBER_MOTOR =     new CanPort(0);
+    public static final CanPort CLIMBER_MOTOR =     new CanPort(60);
 
     public static final CanPort INTAKE_MOTOR =     new CanPort(0);
-    public static final CanPort WRIST_MOTOR1 =     new CanPort(0);
+    public static final CanPort WRIST_MOTOR1 =     new CanPort(54);
     public static final CanPort WRIST_CAN_PORT =   new CanPort(0);
 
     public static final CanPort INTAKE_CANCODER = new CanPort(0);
